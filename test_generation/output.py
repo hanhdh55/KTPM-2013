ec042f9ef8554e88cb710e7d3ebdf01771ff65b6
DuongHanh <duonghanh.cn68@gmail.com>
2013-10-19 21:01:45 +0700

    Hanh

--- test_generation/output.py
+++ test_generation/output.py
@@ -1,66 +1,90 @@
-# Do Thanh Trung - 10020382
-# output.py
-
 import input
-import itertools
 import unittest
-
-arr = [] # mang chua cac gia tri dai dien
-arr2 = [] # mang chua cac khoang gia tri
-docstring = input.main.__doc__
-
-# dua cac dau vao vao mang
-
-i = 0
-j = -1
-k = 0
-try:
-    while(i<len(docstring)):
-        if((i+1)<len(docstring) and docstring[i+1]==':'): 
-            j+=1
-            arr.append([])
-            arr2 = []
-            k = 0
-        tmp = ''
-        if(docstring[i]=='['): 
-            arr2.append([])
-            i = i+1
-            while(docstring[i]!=';'): # bien dau
-                tmp += docstring[i]
-                i+=1
-            a = int(tmp)
-            tmp = ''
-            i+=1
-            while(docstring[i]!=']'): # bien cuoi
-                tmp += docstring[i]
-                i+=1
-            b = int(tmp)
-            arr[j].append((a+b)/2) #them gia tri dai dien cho moi khoang
-            tmp = ''
-            
-            #xet lop tuong duong
-            arr2[k].append(a)
-            arr2[k].append(b)
-            q = 0
-            while(q < k):
-                if(arr2[q][0]<=a<=arr2[q][1] or arr2[q][0]<=a<=arr2[q][1]
-                   or a<=arr2[q][0]<=b or a<=arr2[q][1]<=b):
-                    raise Exception("wrong input")
-                q+=1
-            k+=1 
-        i+=1
+import re
+import itertools
     
-    class output(unittest.TestCase):
-        pass
-    def test_generator(*args):
-        def test(self):
-            self.assertEqual(input.main(*args),1)
-        return test
-    if __name__ == '__main__':
-        for element in itertools.product(*arr):
-            test_name = 'test_' + str(element)
-            test = test_generator(*element)
-            setattr(output, test_name, test)
+#kiem tra 1 string co phai so hay khong
+def check_int(s):
+    if len(s) == 0:
+        return False
+    elif (s[0] == '-'):
+        return s[1:].isdigit()
+    else:
+        return s.isdigit()
+
+
+#kiem tra mien dau vao co hop le hay khong
+def validinput(arr):
+    check = 0
+    for i in range(len(arr)):
+        n = len(arr[i])/2
+        for j in range(n):
+            for k in range(j+1, n):
+                if (arr[i][2*j] <= arr[i][2*k] and arr[i][2*j+1] >= arr[i][2*k]) or (arr[i][2*j] >= arr[i][2*k] and arr[i][2*k+1] >= arr[i][2*j]):
+                   return False
+    return True
+
+                   
+#doc cac gia tri dau vao, neu hop le thi tach thanh cac lop tuong duong
+#va tra ve gia tri dai dien cho moi lop, la gia tri giua.
+#Hoac return false neu khong hop le
+def read():
+    doc = input.main.__doc__
+
+    variables = (doc.split( ))
+    num = len(variables) #so cac bien'
+    boundary = [] #mang luu cac bien' va cac bien cua no
+    
+    for i in range(num):
+        strv = re.split('[:;\]\[]', variables[i])
+        k = 0
+        boundary.append([])
+        for j in range(len(strv)):
+            if(check_int(strv[j])):
+                boundary[i].append(int(strv[j]))
+                k += 1
+    if(validinput(boundary)):
+        tb = [] #luu gia tri dai dien cho tung khoang cua bien
+        for i in range(num):
+            tb.append([])
+            n = len(boundary[i])/2
+            for j in range(n):
+                gt = (boundary[i][2*j] + boundary[i][2*j+1])/2
+                tb[i].append(gt)
+        return tb
+    else:
+        return False
+
+class TestSequense(unittest.TestCase):
+    pass
+
+def test_generator(*a):
+    def test(self):
+        self.assertEqual(input.main(*a), "aaa")
+    return test
+            
+
+if __name__ == '__main__':
+    if (read() != False):
+        # luu cac gia tri test case
+        tb = read()
+        arr = []
+        i = 0
+        for element in itertools.product(*tb):
+            arr[len(arr):]=[element]
+            i+=1
+            
+        #tao va tu dong run test    
+        j = 0
+        for t in arr:
+            test_name = 'test_%s' % str(j)
+            test = test_generator(*t)
+            setattr(TestSequense, test_name, test)
+            j+=1
         unittest.main()
-except Exception as e:
-    raise
+    else:
+        raise Exception ('wrong input')
+         
+
+
+
